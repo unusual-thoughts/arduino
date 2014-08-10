0f8aef0789ebbed9944f9bea50bb0df86b55550a
unusual-thoughts <henritonchain@gmail.com>
2014-08-11 00:29:39 +0200

    cosmetic changes
    looks prettier now

--- LEDboard/LEDboard.ino
+++ LEDboard/LEDboard.ino
@@ -5,8 +5,8 @@
 int RECV_PIN = 3;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
-uint8_t channel=0;
-LEDBoard board(4095,2048,0);
+uint8_t channel = 0;
+LEDBoard board(4095, 2048, 0);
 
 void setup()
 {
@@ -23,24 +23,22 @@ void setup()
 void loop()
 {
   //Serial.println("loop");
-  if (irrecv.decode(&results)) {
+  if (irrecv.decode(&results))
+  {
     Serial.println(results.value, HEX);
-    board.transitionHSV(channel,(channel+1)%5,random(4096),4095,4095);
+    board.transitionHSV(channel, (channel + 1) % 5, random(4096), 4095, 4095);
     //board.getLED((channel+1)%5)->setHSV(4000,4095,1000);
-    Serial.println(board.getLED((channel+1)%5)->getR());
-    Serial.println(board.getLED((channel+1)%5)->getG());
-    Serial.println(board.getLED((channel+1)%5)->getB());
-    board.setLED((channel+1)%5);
+    Serial.println(board.getLED((channel + 1) % 5)->getR());
+    Serial.println(board.getLED((channel + 1) % 5)->getG());
+    Serial.println(board.getLED((channel + 1) % 5)->getB());
+    board.setLED((channel + 1) % 5);
     Tlc.update();
-
     
     channel++;
-    channel%=5;
+    channel %= 5;
 
     Tlc.clear();
     irrecv.resume(); // Receive the next value
 
-   }
-  
-
+  }
 }
